05f1fec2a7050c8d29612ba7584646f8de14475f
marumo333 <seito5026junior@icloud.com>
2025-05-20 23:06:26 +0900

    style: 目に優しい配色に変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          light: 'rgb(191, 219, 254)', // より薄い青色
+          DEFAULT: 'rgb(96, 165, 250)', // 薄い青色
+          dark: 'rgb(59, 130, 246)', // アクセント用の青色
+        },
+        background: 'rgb(255, 255, 255)',
+        text: 'rgb(0, 0, 0)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
